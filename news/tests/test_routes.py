ff5f8b929424d9cc6e59a402dae6b09f96cf1782
KINikita <nikitakir11@gmail.com>
2025-04-14 19:28:37 +0300

    2_nd commit

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -1,18 +1,27 @@
-# news/tests/test_routes.py
-# Импортируем класс HTTPStatus.
 from http import HTTPStatus
 
 from django.test import TestCase
-# Импортируем функцию reverse().
 from django.urls import reverse
+
+from news.models import News
 
 
 class TestRoutes(TestCase):
 
-    def test_home_page(self):
-        # Вместо прямого указания адреса
-        # получаем его при помощи функции reverse().
-        url = reverse('news:home')
-        response = self.client.get(url)
-        # Проверяем, что код ответа равен статусу OK (он же 200).
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
